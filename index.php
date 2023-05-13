d2a415798efdb8c57c5db65905e328e5df2ddfe1
KLaZ <muhabdanulikhlas0983@gmail.com>
2023-05-13 14:53:02 +0700

    cicil halaman produk

--- index.php
+++ index.php
@@ -6,14 +6,17 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pareanom</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/styles/produk.css">
 </head>
 
 <body>
     <main>
         <nav>
-            <header>               
-                <a href="index.php">  <!--? Halaman Produk  -->
+            <header>
+                <a href="index.php">
+                    <!--? Halaman Produk  -->
                     <div class="garis-bawah-gambar">
                         <img src="assets/image/Logo Pareanom.png" alt="Logo Pareanom">
                     </div>
@@ -22,7 +25,7 @@
                 <h1>CHICKEN BAR</h1>
             </header>
             <ul>
-                <a href="halamanProduk.php">
+                <a href="index.php">
                     <li>
                         <div class="kotak-ikon">
                             <img src="assets/image/Produk.png">
@@ -56,9 +59,74 @@
                 </a>
             </ul>
         </nav>
-        <section>
+        <article>
+            <section class="produk">
+                <h2>PRODUK</h2>
+                <div class="container-button-tambahProduk">
+                    <a href="tambahProduk.php">
+                        <button>Tambah</button>
+                    </a>
+                </div>
+                <div class="container-card-produk">
+                    <div class="card-produk">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita ab aspernatur deleniti illo
+                        minus dignissimos id culpa recusandae accusamus blanditiis.
+                    </div>
+                    <div class="card-produk">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita ab aspernatur deleniti illo
+                        minus dignissimos id culpa recusandae accusamus blanditiis.
+                    </div>
+                    <div class="card-produk">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita ab aspernatur deleniti illo
+                        minus dignissimos id culpa recusandae accusamus blanditiis.
+                    </div>
+                    <div class="card-produk">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita ab aspernatur deleniti illo
+                        minus dignissimos id culpa recusandae accusamus blanditiis.
+                    </div>
+                    <div class="card-produk">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita ab aspernatur deleniti illo
+                        minus dignissimos id culpa recusandae accusamus blanditiis.
+                    </div>
+                </div>
+            </section>
+            <div class="container-kanan">
+                <section class="addOns">
+                    <h2>ADDONS</h2>
+                    <div class="container-button-tambahAddOns">
+                        <a href="tambahAddOns.php">
+                            <button>Tambah</button>
+                        </a>
+                    </div>
+                    <div class="container-card-addOns">
+                        <div class="card-addOns">
+                            Lorem ipsum, dolor sit amet 
+                        </div>
+                        <div class="card-addOns">
+                            Lorem ipsum, dolor sit amet 
+                        </div>
+                        <div class="card-addOns">
+                            Lorem ipsum, dolor sit amet 
+                        </div>
+                        <div class="card-addOns">
+                            Lorem ipsum, dolor sit amet 
+                        </div>
+                        <div class="card-addOns">
+                            Lorem ipsum, dolor sit amet 
+                        </div>
+                        <div class="card-addOns">
+                            Lorem ipsum, dolor sit amet 
+                        </div>
+                        
+                        
+                    </div>
+                </section>
+                <section class="total">
+                    <h2>TOTAL</h2>
 
-        </section>
+                </section>
+            </div>
+        </article>
     </main>
 </body>
 
