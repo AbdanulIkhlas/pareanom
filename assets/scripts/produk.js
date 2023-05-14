6e73d814ed2cc1cb5cacefbea107bdcdec4ec426
KLaZ <muhabdanulikhlas0983@gmail.com>
2023-05-14 22:59:57 +0700

    lanjut nyicil halaman produk

--- assets/scripts/produk.js
+++ assets/scripts/produk.js
@@ -1,15 +1,146 @@
-const cardProduk = document.querySelectorAll(".card-produk");
+// //! deklarasi variabel untuk popup 
+// const cardProduk = document.querySelectorAll(".card-produk");
+// const cardAddOns = document.querySelectorAll(".card-addOns");
+// const popUp = document.querySelector(".container-popUp");
+// const popUpClose = document.querySelector(".close");
+// const popUpImg = document.querySelector(".container-popUp .foto img");
+// const popUpNamaProduk = document.querySelector(".container-popUp .nama-produk p");
+// const popUpHargaProduk = document.querySelector(".container-popUp .harga-produk p");
+// const jumlahPembelian = document.querySelector(".jumlah-pembelian");
+// const jumlahText = jumlahPembelian.querySelector("p");
+// const plusButton = jumlahPembelian.querySelector(".plus");
+// const minusButton = jumlahPembelian.querySelector(".minus");
+// let jumlah = 1; //? Jumlah awal pembelian
 
-cardProduk.forEach(function (card) {
-    card.addEventListener("click", function () {
-        //? Mengambil value saat card di klik
-        let imgSrc = card.querySelector(".gambar-produk img").getAttribute("src");
-        let namaProduk = card.querySelector(".nama-produk p").textContent;
-        let hargaProduk = card.querySelector(".harga-produk p").textContent;
+// //! event untuk menampilkan popUp setelah card produk di klik
+// cardProduk.forEach(function (card) {
+//     card.addEventListener("click", function () {
+//         //? Mengambil value saat card di klik
+//         let imgSrc = card.querySelector(".gambar-produk img").getAttribute("src");
+//         let namaProduk = card.querySelector(".nama-produk p").textContent;
+//         let hargaProduk = card.querySelector(".harga-produk p").textContent;
 
-        // Lakukan sesuatu dengan nilai yang diambil
-        console.log(imgSrc);
-        console.log(namaProduk);
-        console.log(hargaProduk);
+//         //? set value yang di dapat dari card ke popup
+//         popUpImg.src = imgSrc;
+//         popUpNamaProduk.innerHTML = namaProduk;
+//         popUpHargaProduk.innerHTML = hargaProduk;
+
+//         //? menampilkan popUp
+//         popUp.style.display = 'block';
+//     });
+// });
+
+// //! event untuk menampilkan popUp setelah card addOns di klik
+// cardAddOns.forEach(function (card) {
+//     card.addEventListener("click", function () {
+//         //? Mengambil value saat card di klik
+//         let imgSrc = card.querySelector(".gambar-addOns img").getAttribute("src");
+//         let namaProduk = card.querySelector(".nama-addOns p").textContent;
+//         let hargaProduk = card.querySelector(".harga-addOns p").textContent;
+
+//         //? set value yang di dapat dari card ke popup
+//         popUpImg.src = imgSrc;
+//         popUpNamaProduk.innerHTML = namaProduk;
+//         popUpHargaProduk.innerHTML = hargaProduk;
+
+//         //? menampilkan popUp
+//         popUp.style.display = 'block';
+//     });
+// });
+
+// //! Event listener untuk menutup popup
+// popUpClose.addEventListener('click', function () {
+//     // Sembunyikan popup
+//     popUp.style.display = 'none';
+// });
+
+
+// 
+// function updateJumlah() {
+//     jumlahText.textContent = jumlah;
+// }
+
+// //! Event listener untuk tombol plus
+// plusButton.addEventListener("click", function () {
+//     jumlah++; //? Menambah jumlah
+//     updateJumlah(); //? Memperbarui tampilan jumlah
+// });
+
+// //! Event listener untuk tombol minus
+// minusButton.addEventListener("click", function () {
+//     if (jumlah > 1) {
+//         jumlah--; //? Mengurangi jumlah jika lebih dari 1
+//         updateJumlah(); //? Memperbarui tampilan jumlah
+//     }
+// });
+
+// //? Memanggil fungsi awal untuk menampilkan jumlah awal
+// updateJumlah();
+
+
+
+const cardProduk = document.querySelectorAll(".card-produk, .card-addOns");
+const popUp = document.querySelector(".container-popUp");
+const popUpClose = document.querySelector(".close");
+const okButton = document.querySelector(".container-popUp button");
+const popUpImg = document.querySelector(".container-popUp .foto img");
+const popUpNamaProduk = document.querySelector(".container-popUp .nama-produk p");
+const popUpHargaProduk = document.querySelector(".container-popUp .harga-produk p");
+const jumlahPembelian = document.querySelector(".jumlah-pembelian");
+const jumlahText = jumlahPembelian.querySelector("p");
+const plusButton = jumlahPembelian.querySelector(".plus");
+const minusButton = jumlahPembelian.querySelector(".minus");
+let jumlah = 1;
+
+//! Fungsi untuk menampilkan popUp
+function tampilkanPopUp(imgSrc, namaProduk, hargaProduk) {
+    popUpImg.src = imgSrc;
+    popUpNamaProduk.innerHTML = namaProduk;
+    popUpHargaProduk.innerHTML = hargaProduk;
+    popUp.style.display = 'block';
+}
+
+//! Fungsi untuk menampilkan jumlah pembelian
+function updateJumlah() {
+    jumlahText.textContent = jumlah;
+}
+
+//! Fungsi untuk menampilkan popup ketika cards di klik
+function tambahCardEventListener(cards) {
+    cards.forEach(function (card) {
+        card.addEventListener("click", function () {
+            let imgSrc = card.querySelector(".gambar-produk img, .gambar-addOns img").getAttribute("src");
+            let namaProduk = card.querySelector(".nama-produk p, .nama-addOns p").textContent;
+            let hargaProduk = card.querySelector(".harga-produk p, .harga-addOns p").textContent;
+            tampilkanPopUp(imgSrc, namaProduk, hargaProduk);
+        });
     });
+}
+
+tambahCardEventListener(cardProduk);
+
+popUpClose.addEventListener('click', function () {
+    popUp.style.display = 'none';
+    jumlah = 1;
+    jumlahText.textContent = jumlah;
 });
+
+okButton.addEventListener('click', function () {
+    popUp.style.display = 'none';
+    jumlah = 1;
+    jumlahText.textContent = jumlah;
+});
+
+plusButton.addEventListener("click", function () {
+    jumlah++;
+    updateJumlah();
+});
+
+minusButton.addEventListener("click", function () {
+    if (jumlah > 1) {
+        jumlah--;
+        updateJumlah();
+    }
+});
+
+updateJumlah();
--- index.php
+++ index.php
@@ -219,6 +219,16 @@
                                 <div class="jumlah">
                                     <p>1</p>
                                 </div>
+                                <div class="tombol-delete">
+                                    <div class="minus">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="white"
+                                            class="bi bi-dash-circle" viewBox="0 0 16 16">
+                                            <path
+                                                d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                                            <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z" />
+                                        </svg>
+                                    </div>
+                                </div>
                             </div>
                             <div class="card-produk-dibeli">
                                 <div class="foto">
@@ -230,6 +240,16 @@
                                 <div class="jumlah">
                                     <p>1</p>
                                 </div>
+                                <div class="tombol-delete">
+                                    <div class="minus">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="white"
+                                            class="bi bi-dash-circle" viewBox="0 0 16 16">
+                                            <path
+                                                d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                                            <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z" />
+                                        </svg>
+                                    </div>
+                                </div>
                             </div>
                             <div class="card-produk-dibeli">
                                 <div class="foto">
@@ -240,6 +260,16 @@
                                 </div>
                                 <div class="jumlah">
                                     <p>10</p>
+                                </div>
+                                <div class="tombol-delete">
+                                    <div class="minus">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="white"
+                                            class="bi bi-dash-circle" viewBox="0 0 16 16">
+                                            <path
+                                                d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                                            <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z" />
+                                        </svg>
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -256,15 +286,42 @@
     </main>
     <article class="container-popUp">
         <section>
+            <div class="close">
+                <svg xmlns="http://www.w3.org/2000/svg" width="38" height="38" fill="white" class="bi bi-x-circle"
+                    viewBox="0 0 16 16">
+                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                    <path
+                        d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+                </svg>
+            </div>
             <div class="foto">
-                <img src="assets/image/Nasi.png">
+                <img src="">
             </div>
             <div class="nama-produk">
-                <p>NASI</p>
+                <p></p>
             </div>
-            <div class="jumlah">
-                <p>10</p>
+            <div class="harga-produk">
+                <p></p>
             </div>
+            <div class="jumlah-pembelian">
+                <div class="minus">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="white"
+                        class="bi bi-dash-circle" viewBox="0 0 16 16">
+                        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                        <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z" />
+                    </svg>
+                </div>
+                <p>1</p>
+                <div class="plus">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="white"
+                        class="bi bi-plus-circle" viewBox="0 0 16 16">
+                        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                        <path
+                            d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
+                    </svg>
+                </div>
+            </div>
+            <button>OK</button>
         </section>
     </article>
     <script src="assets/scripts/produk.js"></script>
