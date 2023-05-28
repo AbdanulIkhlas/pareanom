a1f17dbbf1d6f4166fc7954858b4be9791be237b
KLaZ <muhabdanulikhlas0983@gmail.com>
2023-05-28 09:01:51 +0700

    menambahkan function untuk mengubah format tanggal

--- database.php
+++ database.php
@@ -10,3 +10,10 @@ $connect = new mysqli($hostname, $username, $password, $database);
 if ($connect->connect_error) {
     die('Maaf koneksi gagal: ' . $connect->connect_error);
 }
+
+//! Mengubah format tanggal
+function formatTanggal($date) 
+{
+    $datetime = DateTime::createFromFormat('Y-m-d', $date);
+    return $datetime->format('d-m-Y');
+}
