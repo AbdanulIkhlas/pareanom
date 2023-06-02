ba196e7fdc1c183ad76ce4ca0f80a0d655ef74a8
KLaZ <muhabdanulikhlas0983@gmail.com>
2023-06-02 07:34:36 +0700

    melanjutkan backend menu reject

--- DBInsert_reject.php
+++ DBInsert_reject.php
@@ -24,14 +24,21 @@ foreach ($dataArray as $data) {
     $queryReject = mysqli_query($connect, "SELECT id_produk_terjual FROM produk_terjual WHERE nama_produk_terjual = '$nama_produk_reject' 
     AND harga_satuan = $harga_satuan AND jumlah_produk_terjual = $jumlah_produk_reject AND tanggal_terjual = '$tanggal_reject'");
 
+    echo "id produk 11 : ". $id_produk_terjual;
 
     while($row = mysqli_fetch_array($queryReject)){
         $id_produk_terjual = $row['id_produk_terjual'];
+        echo "id produk 11 : ". $id_produk_terjual;
 
         //! memasukkan data produk reject ke dalam tabel reject
         $queryInsertReject = mysqli_query($connect, "INSERT INTO reject VALUES
         ( '','$id_produk_terjual','$jumlah_produk_reject','$tanggal_reject', '$keterangan')")
         or die(mysqli_error($connect));
+
+        echo "id_produk_terjual : ". $id_produk_terjual . "<br>";
+        echo "jumlah_produk_reject : ". $jumlah_produk_reject . "<br>";
+        echo "tanggal_reject : ". $tanggal_reject . "<br>";
+        echo "keterangan : ". $keterangan . "<br>";
 
         if ($queryInsertReject) {
             $checked = true;
@@ -55,9 +62,9 @@ if ($checked) {
     $totalSaldo = $dataSaldo['total_saldo'] - $harga_satuan;
     $queryUpdateSaldo = mysqli_query($connect, "UPDATE total_saldo set total_saldo = '$saldo'");
 
-    header("location:halamanReject.php?pesan=pembelian_berhasil");
+    header("location:DBInsert_reject.php?pesan=reject_berhasil");
 } else {
-    header("location:halamanReject.php?pesan=pembelian_gagal");
+    header("location:DBInsert_reject.php?pesan=reject_gagal");
 }
 
 //! Menutup connect ke database
