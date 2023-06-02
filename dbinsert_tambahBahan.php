c2e4d7fb53b999da8e69410ebd0498b6607f39d1
KLaZ <muhabdanulikhlas0983@gmail.com>
2023-06-02 22:54:22 +0700

    menambahkan fitur notifikasi untuk menambahkan bahan baku

--- dbinsert_tambahBahan.php
+++ dbinsert_tambahBahan.php
@@ -13,9 +13,10 @@ $sql = "INSERT INTO bahan_baku (nama_bahan, stok_bahan, satuan_bahan, jenis_baha
 // Eksekusi query
 if (mysqli_query($connect, $sql)) {
   // Jika berhasil, arahkan pengguna ke halaman halamanBahan.php
-  header("Location: halamanBahan.php");
+  header("Location: halamanBahan.php?pesanBerhasil=berhasil_insert_bahanBaku");
   exit(); // Hentikan eksekusi skrip setelah mengarahkan pengguna
 } else {
   // Jika terjadi kesalahan, tampilkan pesan error
   echo "Gagal menambahkan data bahan: " . mysqli_error($connect);
+  header("Location: halamanBahan.php?pesanGagal=gagal_insert_bahanBaku");
 }
